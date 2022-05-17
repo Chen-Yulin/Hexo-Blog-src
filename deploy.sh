89e204f1311b78be2675d4bace10d771125ed5f0
chenyulin <chenyulin@sjtu.edu.cn>
2022-05-17 23:03:35 +0800

    revise deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,4 @@ cp /mnt/c/Users/18301/OneDrive/Common/SJTU\ File/semester\ 3/VE401/Note/Class-no
 hexo cl
 hexo g
 
-/usr/bin/expect <<EOF
-    set time 30
-    spawn hexo d
-    expect "Username for 'https://github.com': "
-    send "Chen-Yulin\r"
-    expect "Password for 'https://Chen-Yulin@github.com': "
-    send "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\r"
-    expect eof
-EOF
 
