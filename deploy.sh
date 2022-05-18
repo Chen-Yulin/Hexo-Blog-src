8c845d19372b5490413e8bec8a9563a3a0455ab9
chenyulin <chenyulin@sjtu.edu.cn>
2022-05-18 22:43:20 +0800

    revise ./deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ cp /mnt/c/Users/18301/OneDrive/Common/SJTU\ File/semester\ 3/VE203/Note/Class-no
 cp /mnt/c/Users/18301/OneDrive/Common/SJTU\ File/semester\ 3/VE401/Note/Class-note.pdf ./source/_posts/VE401-Note/
 hexo cl
 hexo g
-hexo s
+hexo d
 
