89cf590bfdde9a5b0a10aed624b6e232afd2a575
chenyulin <chenyulin@sjtu.edu.cn>
2022-05-21 15:50:11 +0800

    Add VE216-Note

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh 
 cp /mnt/c/Users/18301/OneDrive/Common/SJTU\ File/semester\ 3/VE203/Note/Class-note.pdf ./source/_posts/VE203-Note/
 cp /mnt/c/Users/18301/OneDrive/Common/SJTU\ File/semester\ 3/VE401/Note/Class-note.pdf ./source/_posts/VE401-Note/
+cp /mnt/c/Users/18301/OneDrive/Common/SJTU\ File/semester\ 3/VE216/Note/Class-note.pdf ./source/_posts/VE216-Note/
 hexo cl
 hexo g
 hexo d
